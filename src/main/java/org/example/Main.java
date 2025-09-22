3a557f0ae492c27c072d7c3d1a33ee8c14e125c5
Younes <younes.ahmad@iths.se>
2025-09-22 18:05:26 +0200

    first

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,104 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        String word = "pizza";
+
+        ArrayList<Character> wordState = new ArrayList<Character>();
+        int wrongGuesses = 0;
+
+        for(int i = 0; i < word.length(); i++){
+            wordState.add('_');
         }
+        System.out.println("*************************");
+        System.out.println("Welcome to Java Hangman!");
+        System.out.println("*************************");
+
+        while (wrongGuesses <= 6) {
+            System.out.println(getHangmanArt(wrongGuesses));
+            System.out.print("Word: ");
+            for (char c : wordState) {
+                System.out.print(c + " ");
+            }
+            System.out.println();
+
+            System.out.println("Guess a letter: ");
+            char guess = scanner.next().toLowerCase().charAt(0);
+
+            if (wordState.contains(guess)) {
+                System.out.println("You already guessed that letter!");
+                continue;
+            }
+
+            if (word.indexOf(guess) >= 0) {
+                System.out.println("Correct guess!");
+                for (int i = 0; i < word.length(); i++) {
+                    if (word.charAt(i) == guess) {
+                        wordState.set(i, guess);
+                    }
+                }
+            } else {
+                wrongGuesses++;
+                System.out.println("Wrong guess! Remaining: " + (6 - wrongGuesses));
+            }
+
+            if (!wordState.contains('_')) {
+                System.out.println("Congratulations! You guessed the word: " + word + " 🎉");
+                break;
+            }
+        }
+
+        if (wrongGuesses >= 6) {
+            System.out.println(getHangmanArt(wrongGuesses));
+            System.out.println("Game over! The word was: " + word);
+        }
+        scanner.close();
+    }
+    static String getHangmanArt(int wrongGuesses){
+
+        return switch(wrongGuesses){
+            case 0 -> """
+                    
+                    
+                    
+                    """;
+            case 1 -> """
+                       o
+                    
+                    
+                    """;
+            case 2 -> """
+                       o
+                       |
+                    
+                    """;
+            case 3 -> """
+                       o
+                      /|
+                    
+                    """;
+            case 4 -> """
+                       o
+                      /|\\
+                    
+                    """;
+            case 5 -> """
+                       o
+                      /|\\
+                      /
+                    """;
+            case 6 -> """
+                       o
+                      /|\\
+                      / \\
+                    """;
+            default -> "";
+        };
     }
 }
